dcb701c4e083554568955227ed5c2b775745c7d4
xuzhen <1712954704@qq.com>
2020-03-12 23:48:57 +0800

    修改上传

--- app/Http/Controllers/Tool/UploadController.php
+++ app/Http/Controllers/Tool/UploadController.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers\Tool;
 use App\Http\Controllers\Controller;
 use App\Handlers\UploadHandler;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class UploadController extends Controller
 {
 	//上传视频
 //	public function video(Request $request, VideosUploadHandler$uploader)
-	public function video(Request $request, UploadHandler $uploader)
+	public function video(Request $request)
 	{
 		// 初始化返回数据，默认是失败的
 		$data = [
@@ -29,7 +30,7 @@ class UploadController extends Controller
 		}
 		if ($file = $request->file) {
 			// 保存图片到本地
-			$result = $uploader->save($request->file, 'brief', 'admin', $type);
+			$result = $this->save($request->file, 'brief', 'admin', $type);
 			// 图片保存成功的话
 			if ($result) {
 				$data['file_path'] = $result['path'];
@@ -41,5 +42,59 @@ class UploadController extends Controller
 		}
 		
 		return $data;
+	}
+	
+	protected $allowed_ext = ["png", "jpg", "gif", 'jpeg','bmp'];
+	
+	public function save($file, $folder, $file_prefix,$type='')
+	{
+		$file_size = 2097152;//固定文件大小2MB
+		//限制上传文件的大小
+//			if ($file->getClientSize() >= $file_size) {
+//				return layui_json(404, '请上传两兆以内的文件');
+//			}
+		//$file->getClientSize()获取文件大小
+		// 构建存储的文件夹规则，值如：uploads/images/avatars/201709/21/
+		$folder_name = "upload/video/drama/$folder/" . date("Ym/d", time());
+		// 文件具体存储的物理路径，`public_path()` 获取的是 `public` 文件夹的物理路径。
+		// 值如：/home/vagrant/Code/lara/public/uploads/images/avatars/201709/21/
+		$upload_path = public_path() . '/' . $folder_name;
+		
+		// 获取文件的后缀名，因图片从剪贴板里黏贴时后缀名为空，所以此处确保后缀一直存在
+		$extension = strtolower($file->getClientOriginalExtension()) ?: 'png';
+		
+		// 拼接文件名，加前缀是为了增加辨析度，前缀可以是相关数据模型的 ID
+		// 值如：1_1493521050_7BVc9v9ujP.png
+//			Str::random(12)
+//			$filename = $file_prefix . '_' . time() . '_' . str_random(10) . '.' . $extension;
+		$filename = $file_prefix . '_' . time() . '_' . Str::random(10) . '.' . $extension;
+		
+		// 如果上传的不是图片将终止操作
+//			if (!in_array($extension, $this->allowed_ext)) {
+//				return layui_json(404, '上传文件格式不正确!');
+//			}
+		
+		// 将图片移动到我们的目标存储路径中
+		$re =  $file->move($upload_path, $filename);
+		
+		if(!$re){
+			return layui_json(404, '上传失败!');
+		}
+//        if($type == "banner"){
+//            // 判断宽高 682*246px
+//            $editor = Grafika::createEditor();
+//            $editor->open( $image,  $upload_path.'/'.$filename);
+//            $height = $image->getHeight();
+//            $width = $image->getWidth();
+//
+//            if($height != 246 || $width != 682){
+//                unlink($upload_path.'/'.$filename);
+//                return false;
+//            }
+//        }
+		
+		return [
+			'path' => config('app.url') . "$folder_name/$filename"
+		];
 	}
 }
